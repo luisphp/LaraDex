20deb3427038d2056c8d15dd7785525fa63dc0c0
Luis H <oneillvilla@gmail.com>
2019-02-04 21:15:51 -0430

    Cambios para el CRUD

--- resources/views/trainer/create.blade.php
+++ resources/views/trainer/create.blade.php
@@ -9,11 +9,13 @@
 <h1> Cargar entrenador </h2>
 
 
-<form class="form-horizontal margin-top 1-2" action="/action_page.php">
+
+
+<form class="form-horizontal" action="/action_page.php">
 
 
   <div class="form-group">
-    <label class="control-label col-sm-2" for="name">Nombre</label>
+    <label class="control-label col-sm-1" for="name">Nombre</label>
     <div class="col-sm-10">
       <input type="Nombre" class="form-control" id="nombre" placeholder="Ingrese Nombre">
     </div>
@@ -21,16 +23,16 @@
 
 
   <div class="form-group">
-    <label class="control-label col-sm-2" for="name">Correo</label>
+    <label class="control-label col-sm-1" for="name">Correo</label>
     <div class="col-sm-10"> 
       <input type="Apellido" class="form-control" id="correo" placeholder="Ingrese Correo">
     </div>
   </div>
 
   
-  </div>
+  
   <div class="form-group"> 
-    <div class="col-sm-offset-2 col-sm-10">
+    <div class="col-sm-offset-2 col-sm-1">
       <button type="submit" class="btn btn-primary">Guardar</button>
     </div>
   </div>
