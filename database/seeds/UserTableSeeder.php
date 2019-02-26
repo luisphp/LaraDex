ab8687bd0ffdac62917a6adb4555c3b936d73251
Luis Hurtado <luis.hurtado@linio.com>
2019-02-26 18:59:43 -0400

    Add Roles and Users Seeds and Migrations

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $role_user = Role::where('name','user')->first();
+        $role_admin = Role::where('name','Admin')->first();
+
+        $user = new User();
+        $user->name = "User";
+        $user->email = "user@gmail.com";
+        $user->password = bcrypt('query');
+        $user->save();
+		$user->roles()->attach($role_user);
+
+		$user = new User();
+        $user->name = "Admin";
+        $user->email = "Admin@gmail.com";
+        $user->password = bcrypt('query');
+        $user->save();
+		$user->roles()->attach($role_user);
+
+    }
+}
